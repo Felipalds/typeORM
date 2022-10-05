74c1b6ef5859546d68ab6cdf02f64b5119f81e25
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2022-10-05 20:07:15 -0300

    type

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata"
 require('dotenv').config()
-import { DataSource } from "typeorm"
+import { DataSource, DataSourceOptions } from "typeorm"
 import { User } from "./entity/User"
 import { Photo } from "./entity/Photo"
 import { PhotoMetadata } from "./entity/PhotoMetadata"
@@ -12,12 +12,13 @@ import * as path from 'path'
 
 console.log(process.env.DATABASE_HOST)
 console.log(fs.readFileSync(path.resolve('rds-combined-ca-bundle.pem')))
+console.log(Boolean(process.env.USE_UNIFIED_TOPOLOGY))
 
 export const AppDataSource = new DataSource({
-    type: process.env.TYPE || 'mongodb',
-    useNewUrlParser: process.env.USE_NEW_URL_PARSER || true,
-    useUnifiedTopology: process.env.USE_UNIFIED_TOPOLOGY || true,
-    url: process.env.DATABASE_HOST,
+    type:'mongodb',
+    useNewUrlParser: Boolean(process.env.USE_NEW_URL_PARSER) || true,
+    useUnifiedTopology: Boolean(process.env.USE_UNIFIED_TOPOLOGY) || true,
+    //url: process.env.DATABASE_HOST,
     host: process.env.DATABASE_HOST,
     port: Number(process.env.DATABASE_PORT),
     database: process.env.DATABASE_NAME,
@@ -25,13 +26,13 @@ export const AppDataSource = new DataSource({
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     authSource: process.env.DATABASE_AUTH_SOURCE,
-    synchronize: process.env.SYNCHRONIZE || true,
-    logging: process.env.LOGGING || false,
+    synchronize: Boolean(process.env.SYNCHRONIZE) || true,
+    logging: Boolean(process.env.LOGGING) || false,
     entities: [Photo, User, PhotoMetadata],
     migrations: [],
     subscribers: [],
-    ssl: process.env.SSL || true,
+    ssl: Boolean(process.env.SSL) || true,
     sslCA: fs.readFileSync(path.resolve('rds-combined-ca-bundle.pem')),
-    sslValidate: process.env.SSL_VALIDATE || false,
-    retryWrites: process.env.RETRY_WRITES || false
+    sslValidate: Boolean(process.env.SSL_VALIDATE) || false,
+    retryWrites: Boolean(process.env.RETRY_WRITES) || false
 })
